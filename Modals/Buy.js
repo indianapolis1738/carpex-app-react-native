7b92ed54f42948ac3280efdc34231049202d0662
indianapolis1738 <tobilobad1738@gmail.com>
2023-03-24 22:28:12 +0100

    upgrading buy modal

---
+++ Modals/Buy.js
@@ -0,0 +1,16 @@
+ import { StyleSheet, Text, View } from 'react-native'
+ import React from 'react'
+ 
+ const Buy = () => {
+   return (
+     <View>
+       
+     </View>
+   )
+ }
+ 
+ export default Buy
+ 
+ const styles = StyleSheet.create({
+
+ })
