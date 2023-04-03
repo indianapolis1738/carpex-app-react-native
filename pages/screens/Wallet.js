97a141dd17aaa448acc1f7b9b9d49cd5ced22d52
indianapolis1738 <tobilobad1738@gmail.com>
2023-04-03 22:01:31 +0100

    bug

--- pages/screens/Wallet.js
+++ pages/screens/Wallet.js
@@ -19,11 +19,11 @@ const DATA = [
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
+    title: '2',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    title: '3',
   },
 ];
 
